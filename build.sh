1851a6d2e02a993a36cd02c0681fc0e87647b5f3
Evan Overman <evanrileyoverman@gmail.com>
2023-01-08 13:09:44 -0800

    build to static library

--- build.sh
+++ build.sh
@@ -4,7 +4,13 @@
 # is the same name as the folder containing the projects source files.
 
 PROJECT_NAME="$(basename $(pwd))"
-SOURCE_FILES=$(find "./$PROJECT_NAME" -name "*.c")
+SOURCE_FILES=$(find "./$PROJECT_NAME/" -name "*.c")
 mkdir bin -p
-gcc -c $SOURCE_FILES -o "bin/$PROJECT_NAME"
 
+for FILE in $SOURCE_FILES; do
+    gcc -c $FILE -o "bin/$(basename $FILE).o"
+done
+
+OBJECT_FILES=$(find "./bin/" -name "*.o" -printf "%p ")
+
+ar rcs "bin/$PROJECT_NAME.a" $OBJECT_FILES
