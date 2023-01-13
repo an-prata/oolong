585bd8fd8e2d02776390feda1f4fcd823df4a988
Evan Overman <evanrileyoverman@gmail.com>
2023-01-13 09:52:58 -0800

    rename to include "oolong"

--- oolong/screen_buffer.h
+++ oolong/screen_buffer.h
@@ -3,6 +3,7 @@
  * See LICENSE file in repository root for complete license text.
  */
 
+/* This constant is required for the full set of wide character functions. */
 #define __USE_XOPEN
 
 #include <stdlib.h>
@@ -25,29 +26,29 @@ struct oolong_screen_buffer_dimensions_s
 /*
  * Creates and returns a pointer to a new screen buffer.
  */
-oolong_screen_buffer_t* screen_buffer_create(void);
+oolong_screen_buffer_t* oolong_screen_buffer_create(void);
 
 /*
  * Updates the dimensions of the given screen buffer. Existing contents will be
  * realligned to the new dimensions so that they still appear correctly.
  */
-oolong_error_t screen_buffer_update_dimensions(oolong_screen_buffer_t* screen_buffer);
+oolong_error_t oolong_screen_buffer_update_dimensions(oolong_screen_buffer_t* screen_buffer);
 
 /*
  * Gets the dimensions of the given screen buffer.
  */
-oolong_screen_buffer_dimensions_t screen_buffer_get_dimensions(oolong_screen_buffer_t* screen_buffer);
+oolong_screen_buffer_dimensions_t oolong_screen_buffer_get_dimensions(oolong_screen_buffer_t* screen_buffer);
 
 /*
  * Gets a pointer to the contents of the given screen buffer, this may be used
  * to modify the screen buffer's contents within the bounds of its dimensions.
  */
-wchar_t* screen_buffer_get_contents(oolong_screen_buffer_t* screen_buffer);
+wchar_t* oolong_screen_buffer_get_contents(oolong_screen_buffer_t* screen_buffer);
 
 /*
  * Prints the given screen buffer to the terminal, it is recommended that this
  * be used in an alternate terminal "screen" so that when the program exits it
  * doesnt leave behind the output of each frame it rendered.
  */
-oolong_error_t screen_buffer_print(oolong_screen_buffer_t* screen_buffer);
+oolong_error_t oolong_screen_buffer_print(oolong_screen_buffer_t* screen_buffer);
 
