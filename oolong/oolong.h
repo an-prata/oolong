8abf538d684003666e9b62f0d723b0bb55170348
Evan Overman <evanrileyoverman@gmail.com>
2023-01-23 13:04:26 -0800

    add version constants, should've done that before making a release...

--- oolong/oolong.h
+++ oolong/oolong.h
@@ -3,6 +3,10 @@
  * See LICENSE file in repository root for complete license text.
  */
 
+#define OOLONG_VERSION_MAJOR 1
+#define OOLONG_VERSION_MINOR 0
+#define OOLONG_VERSION_PATCH 0
+
 #include "error.h"
 #include "escapes.h"
 #include "keyboard.h"
