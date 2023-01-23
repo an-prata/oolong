c828867e007606f480dc4055ca7d39524c768433
Evan Overman <evanrileyoverman@gmail.com>
2023-01-23 14:51:55 -0800

    one more test case

--- tests/style_set_tests.c
+++ tests/style_set_tests.c
@@ -9,6 +9,7 @@
 SCRUTINY_UNIT_TEST style_set_add_test(void)
 {
 	oolong_style_set_t* style_set = oolong_style_set_create();
+	scrutiny_assert_equal_array(L"", style_set, sizeof(wchar_t), wcslen(style_set));
 
 	oolong_style_set_add(&style_set, OOLONG_STYLE_BACKGROUND_BLACK);
 	scrutiny_assert_equal_array(L"\033[40m", style_set, sizeof(wchar_t), wcslen(style_set));
