f7c1e734d011542ef2d37515f17ddab2e6ca195a
Evan Overman <evanrileyoverman@gmail.com>
2023-01-25 10:28:57 -0800

    make build exit 1 if it fails

--- build.sh
+++ build.sh
@@ -16,6 +16,10 @@ mkdir build -p
 
 for FILE in $SOURCE_FILES; do
     gcc -c $FILE -o "build/$(basename $FILE).o"
+
+    if [ "$?" != "0" ]; then
+        exit 1
+    fi
 done
 
 tar --create --file build/oolong_headers.tar $HEADER_LIST 
@@ -23,3 +27,8 @@ tar --create --file build/oolong_headers.tar $HEADER_LIST
 OBJECT_FILES=$(find "./build/" -name "*.o" -printf "%p ")
 
 ar rcs "build/$PROJECT_NAME.a" $OBJECT_FILES
+
+if [ "$?" != "0" ]; then
+    exit 1
+fi
+
