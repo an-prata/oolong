0de8094b0d579b82fa948ed2f17a5ac96354e061
Evan Overman <evanrileyoverman@gmail.com>
2023-01-27 12:42:36 -0800

    header guards for oolong.h

--- oolong/oolong.h
+++ oolong/oolong.h
@@ -3,6 +3,9 @@
  * See LICENSE file in repository root for complete license text.
  */
 
+#ifndef OOLONG_H
+#define OOLONG_H
+
 #define OOLONG_VERSION_MAJOR 1
 #define OOLONG_VERSION_MINOR 2
 #define OOLONG_VERSION_PATCH 1
@@ -15,4 +18,6 @@
 #include "dialog_view.h"
 #include "page.h"
 #include "styling.h"
+
+#endif // OOLONG_H
 
