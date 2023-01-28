044773adfbaccd1fdc0abe75a697f740aab8510f
Evan Overman <evanrileyoverman@gmail.com>
2023-01-28 15:17:12 -0800

    remove unused screen_buffer functions and types

--- oolong/screen_buffer.c
+++ oolong/screen_buffer.c
@@ -3,20 +3,11 @@
  * See LICENSE file in repository root for complete license text.
  */
 
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include "escapes.h"
 #include "screen_buffer.h"
 
 typedef struct winsize window_size_t;
-
-struct oolong_screen_buffer_s
-{
-    size_t columns;
-    size_t rows;
-    wchar_t* contents;
-};
 
 oolong_screen_buffer_dimensions_t oolong_get_screen_dimensions(void)
 {
@@ -31,94 +22,5 @@ oolong_screen_buffer_dimensions_t oolong_get_screen_dimensions(void)
 
     oolong_screen_buffer_dimensions_t dimensions = { .columns = window_size.ws_col, .rows = window_size.ws_row};
     return dimensions;
-}
-
-oolong_screen_buffer_t* oolong_screen_buffer_create(void)
-{
-    oolong_screen_buffer_t* screen_buffer = malloc(sizeof(oolong_screen_buffer_t));
-    window_size_t window_size;
-
-    if (screen_buffer == NULL)
-    {
-        oolong_error_record(OOLONG_ERROR_NOT_ENOUGH_MEMORY);
-        return NULL;
-    }
-
-    if (ioctl(STDIN_FILENO, TIOCGWINSZ, &window_size) == -1)
-    {
-        oolong_error_record(OOLONG_ERROR_FAILED_IO_READ);
-        return NULL;
-    }
-
-    screen_buffer->columns = window_size.ws_col;
-    screen_buffer->rows = window_size.ws_row;
-    screen_buffer->contents = calloc(screen_buffer->columns * screen_buffer->rows, sizeof(wchar_t));
-
-    if (screen_buffer->contents == NULL)
-    {
-        oolong_error_record(OOLONG_ERROR_NOT_ENOUGH_MEMORY);
-        return NULL;
-    }
-    
-    return screen_buffer;
-}
-
-oolong_error_t oolong_screen_buffer_update_dimensions(oolong_screen_buffer_t* screen_buffer)
-{
-    if (screen_buffer == NULL)
-        return oolong_error_record(OOLONG_ERROR_INVALID_ARGUMENT);
-    
-    window_size_t window_size;
-
-    if (ioctl(STDIN_FILENO, TIOCGWINSZ, &window_size) == -1)
-        return oolong_error_record(OOLONG_ERROR_FAILED_IO_READ);
-
-    screen_buffer->columns = window_size.ws_col;
-    screen_buffer->rows = window_size.ws_row;
-
-    if (screen_buffer->contents == NULL)
-        return OOLONG_ERROR_NONE;
-
-    wchar_t* new_contents = reallocarray(screen_buffer->contents, screen_buffer->columns * screen_buffer->rows, sizeof(wchar_t));
-
-    if (new_contents == NULL)
-        return oolong_error_record(OOLONG_ERROR_NOT_ENOUGH_MEMORY);
-
-    screen_buffer->contents = new_contents;
-    return OOLONG_ERROR_NONE;
-}
-
-oolong_screen_buffer_dimensions_t oolong_screen_buffer_get_dimensions(oolong_screen_buffer_t* screen_buffer)
-{
-    if (screen_buffer == NULL)
-    {
-        oolong_screen_buffer_dimensions_t dimensions = { .columns = 0, .rows = 0 };
-        return dimensions;
-    }
-    
-    oolong_screen_buffer_dimensions_t dimensions = { .columns = screen_buffer->columns, .rows = screen_buffer->rows };
-    return dimensions;
-}
-
-wchar_t* oolong_screen_buffer_get_contents(oolong_screen_buffer_t* screen_buffer)
-{
-    if (screen_buffer == NULL)
-        return NULL;
-
-    return screen_buffer->contents;
-}
-
-oolong_error_t oolong_screen_buffer_print(oolong_screen_buffer_t* screen_buffer)
-{
-    if (screen_buffer == NULL)
-        return oolong_error_record(OOLONG_ERROR_INVALID_ARGUMENT);
-
-    oolong_terminal_clear(stdout);
-    
-    for (size_t i = 0; i < screen_buffer->rows * screen_buffer->columns; i += wcwidth(screen_buffer->contents[i]))
-        printf("%lc", screen_buffer->contents[i]);
-
-    fflush(stdout);
-    return OOLONG_ERROR_NONE;
 }
 
--- oolong/screen_buffer.h
+++ oolong/screen_buffer.h
@@ -9,16 +9,12 @@
 /* This constant is required for the full set of wide character functions. */
 #define __USE_XOPEN
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <wchar.h>
 #include <locale.h>
 #include "error.h"
 
 /* For convinience, any locale can be used but this is whats tested. */
 #define oolong_set_locale() setlocale(LC_CTYPE, "C.UTF-8")
 
-typedef struct oolong_screen_buffer_s oolong_screen_buffer_t;
 typedef struct oolong_screen_buffer_dimensions_s oolong_screen_buffer_dimensions_t;
 
 struct oolong_screen_buffer_dimensions_s
@@ -31,35 +27,6 @@ struct oolong_screen_buffer_dimensions_s
  * The the dimensions of the current terminal screen.
  */
 oolong_screen_buffer_dimensions_t oolong_get_screen_dimensions(void);
-
-/*
- * Creates and returns a pointer to a new screen buffer.
- */
-oolong_screen_buffer_t* oolong_screen_buffer_create(void);
-
-/*
- * Updates the dimensions of the given screen buffer. Existing contents will be
- * realligned to the new dimensions so that they still appear correctly.
- */
-oolong_error_t oolong_screen_buffer_update_dimensions(oolong_screen_buffer_t* screen_buffer);
-
-/*
- * Gets the dimensions of the given screen buffer.
- */
-oolong_screen_buffer_dimensions_t oolong_screen_buffer_get_dimensions(oolong_screen_buffer_t* screen_buffer);
-
-/*
- * Gets a pointer to the contents of the given screen buffer, this may be used
- * to modify the screen buffer's contents within the bounds of its dimensions.
- */
-wchar_t* oolong_screen_buffer_get_contents(oolong_screen_buffer_t* screen_buffer);
-
-/*
- * Prints the given screen buffer to the terminal, it is recommended that this
- * be used in an alternate terminal "screen" so that when the program exits it
- * doesnt leave behind the output of each frame it rendered.
- */
-oolong_error_t oolong_screen_buffer_print(oolong_screen_buffer_t* screen_buffer);
 
 #endif // OOLONG_SCREEN_BUFFER_H
 
